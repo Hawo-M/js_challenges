3c0d9e65a56dfc409d7ab1195e03477931553c9a
+ <+>
2023-10-07 13:48:25 +0100

    i have updated the file

--- functions.js
+++ functions.js
@@ -1,7 +1,12 @@
 function multiply (num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 let result = multiply(2,1)
 console.log(result)
+
+let multiple = (num1, num2) => num1 * num2
+
+let answer = multiple(5, 6)
+console.log(answer)
 
